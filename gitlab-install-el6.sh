7054fdc70a40e2709f8ac3a8ed897c2d7571b5ec
Mattias Ohlsson <mattias.ohlsson@inprose.com>
2012-04-24 15:23:00 +0000

    Add package installation

--- gitlab-install-el6.sh
+++ gitlab-install-el6.sh
@@ -15,3 +15,45 @@ die()
 
 echo "### Check OS (we check if the kernel release contains el6)"
 uname -r | grep "edl6" || die 1 "Not RHEL or CentOS"
+
+
+echo "### Installing packages"
+
+# Install epel-release
+rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-5.noarch.rpm
+
+# Modified list from gitlabhq
+yum install -y \
+make \
+libtool \
+openssh-clients \
+gcc \
+libxml2 \
+libxml2-devel \
+libxslt \
+libxslt-devel \
+python-devel \
+wget \
+readline-devel \
+ncurses-devel \
+gdbm-devel \
+glibc-devel \
+tcl-devel \
+openssl-devel \
+db4-devel \
+byacc \
+httpd \
+gcc-c++ \
+curl-devel \
+openssl-devel \
+zlib-devel \
+httpd-devel \
+apr-devel \
+apr-util-devel \
+sqlite-devel \
+libicu-devel \
+gitolite \
+redis \
+sudo \
+postfix \
+mysql-devel
