a291944294a4ed5df52e88a145864eadad3d3d3d
Mattias Ohlsson <mattias.ohlsson@inprose.com>
2012-04-25 14:55:04 +0000

    Add done message

--- gitlab-install-el6.sh
+++ gitlab-install-el6.sh
@@ -288,3 +288,20 @@ chkconfig httpd on
 
 # Start Apache
 service httpd start
+
+
+echo "### Done ###"
+echo "#"
+if [ "$GL_DATABASE_TYPE" != "sqlite" ]; then
+  # Print MySQL root password instructions
+  echo "# You have your MySQL root password in this file:"
+  echo "# $GL_INSTALL_ROOT/config/database.yml"
+  echo "#"
+fi
+echo "# Point your browser to: 
+echo "# http://$GL_HOSTNAME (or: http://<host-ip>)
+echo "# Default admin username: admin@local.host"
+echo "# Default admin password: 5iveL!fe"
+echo "#"
+echo "# Flattr me if you like this! https://flattr.com/profile/mattiasohlsson"
+echo "###"
