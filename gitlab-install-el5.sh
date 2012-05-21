6b90fa1da5618b0868b60aca8cafb65141b3f9e6
Mattias Ohlsson <mattias.ohlsson@inprose.com>
2012-05-21 12:54:28 +0300

    Use a mirror to download epel-release (download.fedoraproject.org does not work)

--- gitlab-install-el5.sh
+++ gitlab-install-el5.sh
@@ -51,7 +51,7 @@ echo "### Check if we are root"
 echo "### Installing packages"
 
 # Install epel-release
-rpm -Uvh http://download.fedoraproject.org/pub/epel/5/i386/epel-release-5-4.noarch.rpm
+rpm -Uvh http://mirrors.kernel.org/fedora-epel/5/i386/epel-release-5-4.noarch.rpm
 
 # Modified list from gitlabhq
 yum install -y \
