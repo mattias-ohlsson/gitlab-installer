fe2dd0029a4af1bfe56c0f4c14808cb01ecca80f
Mattias Ohlsson <mattias.ohlsson@inprose.com>
2012-07-27 17:02:07 +0200

    add packages from rvm requirements

--- gitlab-install-el6.sh
+++ gitlab-install-el6.sh
@@ -142,6 +142,9 @@ usermod -G git apache
 
 echo "### Installing RVM and Ruby"
 
+# rvm requirements tell us to do this
+yum install -y gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2
+
 # Instructions from https://rvm.io
 curl -L get.rvm.io | bash -s stable 
 
